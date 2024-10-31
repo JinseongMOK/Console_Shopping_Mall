80a08f34859e398afaac295f7b62ef80d439c15b
JinseongMOK <mjs4027@naver.com>
2024-10-31 18:49:07 +0900

    장바구니 초기화 기능, 프로그램 종료 시 한번 더 물어보기 추가

--- Console_Shopping_Mall.dart
+++ Console_Shopping_Mall.dart
@@ -36,7 +36,7 @@ void main() {
         product.forEach((name, price) {
           print('$name: ${price}원');
         });
-
+        break;
       case '2': // 장바구니에 상품 추가
         print('상품 이름을 입력해 주세요.');
         String? productName = stdin.readLineSync();
@@ -51,6 +51,7 @@ void main() {
         } else {
           print('상품명이 입력되지 않았습니다.');
         }
+        // 장바구니에 담은 상품 가격 * 개수 selectPrice에 담기
         if (janbaguni.containsKey('셔츠')) {
           selectPrice.add(janbaguni[productName]! * 45000);
         } else if (janbaguni.containsKey('원피스')) {
@@ -63,16 +64,38 @@ void main() {
           selectPrice.add(janbaguni[productName]! * 5000);
         }
         print('현재 장바구니: ${janbaguni}, ${selectPrice}');
+        break;
       case '3': // 장바구니에 담긴 총 삼품 가격
         // 장바구니에 담긴 수량의 합
-        int totalNum = janbaguni.values.reduce((a, b) => a + b);
-        print('총 주문 개수: ${totalNum}');
+        if (janbaguni.isNotEmpty) {
+          int totalNum = janbaguni.values.reduce((a, b) => a + b);
+          print('총 주문 개수: $totalNum');
+        } else {
+          print('장바구니가 비어 있습니다.');
+        }
         // 장바구니에 담긴 총 상품 가격의 합
-        int totalPrice = selectPrice.reduce((a, b) => a + b);
-        print('장바구니에 담긴 상품의 총 가격: ${totalPrice}');
+        if (janbaguni.isNotEmpty) {
+          int totalPrice = selectPrice.reduce((a, b) => a + b);
+          print('장바구니에 담긴 상품의 총 가격: ${totalPrice}');
+        }
+        print('장바구니를 초기화 하고 싶으면 6, 초기화면으로 돌아가려면 6 을 제외한 숫자를 눌러주세요.');
+        int resetJangbaguni = int.parse(stdin.readLineSync()!);
+        if (resetJangbaguni == 6) {
+          janbaguni.clear();
+          print(janbaguni);
+        }
+        break;
       case '4': // 프로그램 종료
-        print('프로그램을 종료합니다.');
-        return;
+        print('프로그램을 종료하시겠습니까? 종료를 원하시면 5 를 입력해주세요.');
+        String? realExit = stdin.readLineSync();
+
+        if (realExit == '5') {
+          print('프로그램을 종료합니다.');
+          return;
+        } else {
+          print('프로그램을 종료하지 않고, 초기화면으로 돌아갑니다.');
+        }
+        break;
     }
   }
 }
