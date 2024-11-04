9d256583f6c0b6765353eb9558b7b664dd536053
JinseongMOK <mjs4027@naver.com>
2024-11-04 16:51:03 +0900

    리팩토링

--- Console_Shopping_Mall.dart
+++ Console_Shopping_Mall.dart
@@ -1,11 +1,6 @@
 import 'dart:io';
 
-void main() {
-  // 메뉴
-  Set<String> menu = {
-    '[1] 상품 목록 보기 [1-1] 상품 추가하기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료'
-  };
-
+class ShoppingMall {
   // 상품 목록
   Map<String, int> product = {
     '셔츠': 45000,
@@ -16,106 +11,109 @@ void main() {
   };
 
   // 장바구니
-  Map<String, int> janbaguni = {};
+  Map<String, int> jangbaguni = {};
   List<int> selectPrice = [];
 
-  while (true) {
+  void Menu() {
     print(
-        '------------------------------------------------------------------------------------------------------');
-    menu.forEach(print); // 메뉴 출력
-    print(
-        '------------------------------------------------------------------------------------------------------');
+        '[1] 상품 목록 보기 [2] 상품 추가하기 / [3] 장바구니에 담기 / [4] 장바구니에 담긴 상품의 총 가격 보기 / [5] 프로그램 종료');
+  }
 
-    // 사용자로부터 입력 받기
-    print('원하는 서비스의 번호를 입력해 주세요:');
-    String? select_menu = stdin.readLineSync();
+  void Products() {
+    print('상품 목록:');
+    product.forEach((name, price) {
+      print('$name: ${price}원');
+    });
+  }
 
-    switch (select_menu) {
-      case '1': // 상품 목록 보기
-        print('상품 목록:');
-        product.forEach((name, price) {
-          print('${name}: ${price}원');
-        });
-        break;
-      case '1-1':
-        print('추가 할 상품명을 입력해 주세요.');
-        String? newProductName = stdin.readLineSync();
-        print('추가 할 상품의 가격을 입력해 주세요.');
-        int newProductPrice = int.parse(stdin.readLineSync()!);
-        // product에 상품 및 가격 추가
-        if (newProductName != null) {
-          product[newProductName] = newProductPrice;
-          print('${newProductName}: ${newProductPrice}원이 상품 목록에 추가되었습니다.');
-        } else {
-          print('상품 이름이 입력되지 않았습니다.');
-        }
-      case '2': // 장바구니에 상품 추가
-        // 상품명 입력
-        print('상품 이름을 입력해 주세요.');
-        String? productName = stdin.readLineSync();
-        if (!product.containsKey(productName)) {
-          print('준비된 상품이 없습니다.');
-          break;
-        }
-        // 수량 입력
-        if (productName != null) {
-          print('상품 개수를 입력해 주세요.');
-          print('수량을 입력하세요:');
-          int productNumber = int.parse(stdin.readLineSync()!);
-          if (productNumber == 0) {
-            print('1개 이상의 수량을 입력해주세요.');
-          }
-          janbaguni[productName] = productNumber;
-          print('장바구니에 추가되었습니다');
-        } else {
-          print('상품명이 입력되지 않았습니다.');
-        }
-        // 장바구니에 담은 상품 가격 * 개수 selectPrice에 담기
-        if (janbaguni.containsKey('셔츠')) {
-          selectPrice.add(janbaguni[productName]! * 45000);
-        } else if (janbaguni.containsKey('원피스')) {
-          selectPrice.add(janbaguni[productName]! * 30000);
-        } else if (janbaguni.containsKey('반팔티')) {
-          selectPrice.add(janbaguni[productName]! * 35000);
-        } else if (janbaguni.containsKey('반바지')) {
-          selectPrice.add(janbaguni[productName]! * 38000);
-        } else if (janbaguni.containsKey('양말')) {
-          selectPrice.add(janbaguni[productName]! * 5000);
-        }
-        print('현재 장바구니: ${janbaguni}, ${selectPrice}');
-        break;
-      case '3': // 장바구니에 담긴 총 삼품 가격
-        // 장바구니에 담긴 수량의 합
-        if (janbaguni.isNotEmpty) {
-          int totalNum = janbaguni.values.reduce((a, b) => a + b);
-          print('총 주문 개수: ${totalNum}');
-        } else {
-          print('장바구니가 비어 있습니다.');
-        }
-        // 장바구니에 담긴 총 상품 가격의 합
-        if (janbaguni.isNotEmpty) {
-          int totalPrice = selectPrice.reduce((a, b) => a + b);
-          print('장바구니에 담긴 상품의 총 가격: ${totalPrice}');
-        }
-        // 장바구니 초기화
-        print('장바구니를 초기화 하고 싶으면 6, 초기화면으로 돌아가려면 6 을 제외한 숫자를 눌러주세요.');
-        int resetJangbaguni = int.parse(stdin.readLineSync()!);
-        if (resetJangbaguni == 6) {
-          janbaguni.clear();
-          print('장바구니가 초기화 되었습니다.');
-        }
-        break;
-      case '4': // 프로그램 종료
-        print('프로그램을 종료하시겠습니까? 종료를 원하시면 5 를 입력해주세요.');
-        String? realExit = stdin.readLineSync();
-        // 프로그램 종료여부 확인
-        if (realExit == '5') {
-          print('프로그램을 종료합니다.');
-          return;
-        } else {
-          print('프로그램을 종료하지 않고, 초기화면으로 돌아갑니다.');
-        }
-        break;
+  void addProduct() {
+    print('추가 할 상품명을 입력해 주세요.');
+    String? newProductName = stdin.readLineSync();
+    print('추가 할 상품의 가격을 입력해 주세요.');
+    int newProductPrice = int.parse(stdin.readLineSync()!);
+    if (newProductName != null) {
+      product[newProductName] = newProductPrice;
+      print('$newProductName: ${newProductPrice}원이 상품 목록에 추가되었습니다.');
+    } else {
+      print('상품 이름이 입력되지 않았습니다.');
     }
   }
+
+  void addJangbaguni() {
+    print('상품 이름을 입력해 주세요.');
+    String? productName = stdin.readLineSync();
+    if (!product.containsKey(productName)) {
+      print('준비된 상품이 없습니다.');
+      return;
+    }
+    if (productName != null) {
+      print('상품 개수를 입력해 주세요.');
+      int productNumber = int.parse(stdin.readLineSync()!);
+      if (productNumber == 0) {
+        print('1개 이상의 수량을 입력해주세요.');
+        return;
+      }
+      jangbaguni[productName] = productNumber;
+      selectPrice.add(product[productName]! * productNumber);
+      print('장바구니에 추가되었습니다.');
+    }
+  }
+
+  void jangbaguniTotal() {
+    if (jangbaguni.isNotEmpty) {
+      int totalNum = jangbaguni.values.reduce((a, b) => a + b);
+      print('총 주문 개수: $totalNum');
+      int totalPrice = selectPrice.reduce((a, b) => a + b);
+      print('장바구니에 담긴 상품의 총 가격: $totalPrice');
+    } else {
+      print('장바구니가 비어 있습니다.');
+    }
+
+    print('장바구니를 초기화 하고 싶으면 6, 초기화면으로 돌아가려면 6 을 제외한 숫자를 눌러주세요.');
+    int resetJangbaguni = int.parse(stdin.readLineSync()!);
+    if (resetJangbaguni == 6) {
+      jangbaguni.clear();
+      print('장바구니가 초기화 되었습니다.');
+    }
+  }
+
+  void run() {
+    while (true) {
+      Menu();
+      print('원하는 서비스의 번호를 입력해 주세요:');
+      String? selectMenu = stdin.readLineSync();
+
+      switch (selectMenu) {
+        case '1':
+          Products();
+          break;
+        case '2':
+          addProduct();
+          break;
+        case '3':
+          addJangbaguni();
+          break;
+        case '4':
+          jangbaguniTotal();
+          break;
+        case '5':
+          print('프로그램을 종료하시겠습니까? 종료를 원하시면 5 를 입력해주세요.');
+          String? realExit = stdin.readLineSync();
+          if (realExit == '5') {
+            print('프로그램을 종료합니다.');
+            return;
+          } else {
+            print('프로그램을 종료하지 않고, 초기화면으로 돌아갑니다.');
+          }
+          break;
+        default:
+          print('잘못된 입력입니다. 다시 시도해주세요.');
+      }
+    }
+  }
+}
+
+void main() {
+  ShoppingMall shoppingMall = ShoppingMall();
+  shoppingMall.run();
 }
